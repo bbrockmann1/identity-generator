8066246f843b7df55a037e0ff74f76ee9015ccbd
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-09-30 14:58:08 -0600

    allowed for multiple users to be added

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const api = "https://randomuser.me/api/"
 
 function fetchRandomUser(url){
-    fetch(url)
-    .then(resp => resp.json())
-    .then(data => createUserCard(data))
+    document.querySelector("#submitButton").onclick = () => {
+        fetch(url)
+        .then(resp => resp.json())
+        .then(data => createUserCard(data))
+    }
 }
 
 function createUserCard(data){
-    document.querySelector("#submitButton").onclick = () => {
-        const userCard = document.createElement('div')
-        userCard.className = "userCard"
-        userCard.innerHTML = `
-        <img src="${data.results[0].picture.large}">
-        <li>
-        <p>${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}</p>
-        <p>${data.results[0].gender}</p>
-        <p>${data.results[0].dob.date} Age:${data.results[0].dob.age}</p>
-        <p>${data.results[0].location.street.number} ${data.results[0].location.street.name}, ${data.results[0].location.city}, ${data.results[0].location.state}, ${data.results[0].location.country}, ${data.results[0].location.postcode}</p>
-        <p>${data.results[0].email}</p>
-        <p>${data.results[0].phone}</p>
-        <p>${data.results[0].login.username}</p>
-        </li>`
-        document.querySelector("#userCards").append(userCard)
+    const userCard = document.createElement('div')
+    userCard.className = "userCard"
+    userCard.innerHTML = `
+    <img src="${data.results[0].picture.large}">
+    <li>
+    <p>${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}</p>
+    <p>${data.results[0].gender}</p>
+    <p>${data.results[0].dob.date} Age:${data.results[0].dob.age}</p>
+    <p>${data.results[0].location.street.number} ${data.results[0].location.street.name}, ${data.results[0].location.city}, ${data.results[0].location.state}, ${data.results[0].location.country}, ${data.results[0].location.postcode}</p>
+    <p>${data.results[0].email}</p>
+    <p>${data.results[0].phone}</p>
+    <p>${data.results[0].login.username}</p>
+    </li>`
+    document.querySelector("#userCards").append(userCard)
     }
-}
+
 
 fetchRandomUser(api)
