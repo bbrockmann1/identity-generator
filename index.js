1f8fe0429f673b46a3ec0cae59ade4736aa55f3d
Bo Brockmann <bbrockmann1@gmail.com>
2022-10-04 11:20:28 -0600

    added generate seeed functionality to main branch and formating changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const api = "https://randomuser.me/api/"
+const api = "https://randomuser.me/api/";
 
 
 function fetchRandomUser(url){
@@ -15,28 +15,37 @@ function fetchRandomUser(url){
         Promise.all([fetch1, fetch2, fetch3, fetch4])
         .then(fetchArray => {
             fetchArray.forEach(createUserCard);
-        })
-    }
-}
+        });
+    };
+};
+fetchRandomUser(api);
 
 document.querySelector('#clearUsers').onclick = () => {
-    document.querySelectorAll(".userCards").forEach(card => card.remove())
+    document.querySelectorAll(".userCards").forEach(card => {
+        const seedInput = document.getElementById('submitSeed');
+        seedInput.reset();
+        card.remove();
+    })
 }
 
-document.querySelector('#submitSeed').addEventListener('submit', e => fetchSeedUser(e))
+document.querySelector('#submitSeed').addEventListener('submit', e => fetchSeedUser(e));
 
 function fetchSeedUser(event){
-    event.preventDefault()
+    if (event.target.seedInput.value){
+    event.preventDefault();
     fetch(`https://randomuser.me/api/?seed=${event.target.seedInput.value}`)
     .then(res => res.json())
-    .then(data => createUserCard(data))
+    .then(data => createUserCard(data));
+}
+
+    else event.preventDefault();
 }
 
 function createUserCard(data){
 
-    const cardDiv = document.getElementById('listContainer')
-    const userCard = document.createElement('div')
-    userCard.className = 'userCards'
+    const cardDiv = document.getElementById('listContainer');
+    const userCard = document.createElement('div');
+    userCard.className = 'userCards';
 
     const createImg = document.createElement('img');
     const createName = document.createElement('h3');
@@ -47,17 +56,20 @@ function createUserCard(data){
     const createAddress = document.createElement('h3');
     const createSeed = document.createElement('h4')
     const createRemoveButton = document.createElement('button')
-    createRemoveButton.className = 'removeButton'
+    const seedBtn = document.createElement('button');
+    createRemoveButton.className = 'removeButton';
 
     createImg.src = data.results[0].picture.large;
-    createName.textContent = `${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}`
+    createName.textContent = `${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}`;
     createEmail.textContent = `Email:\n${data.results[0].email}`;
     createUsername.textContent = `Username:\n${data.results[0].login.username}`;
     createDOB.textContent = `Age: ${data.results[0].dob.age}\nDOB: ${data.results[0].dob.date.substring(0,10)}`;
-    createNumber.textContent = `Phone Number:\n${data.results[0].phone}`;
-    createAddress.textContent = `Address:\n${data.results[0].location.street.number} ${data.results[0].location.street.name}, ${data.results[0].location.city}, ${data.results[0].location.state}, ${data.results[0].location.country}, ${data.results[0].location.postcode}`;
-    createSeed.textContent = `Seed: \n${data.info.seed}`
-    createRemoveButton.textContent = 'X'
+    createNumber.textContent = `Phone Number:\n${data.results[0].cell}`;
+    createAddress.textContent = `Address:\n${data.results[0].location.street.number} ${data.results[0].location.street.name}\n${data.results[0].location.city}, ${data.results[0].location.state} ${data.results[0].location.postcode}\n${data.results[0].location.country}, `;
+    createSeed.textContent = `${data.info.seed}`;
+    createRemoveButton.textContent = 'X';
+    seedBtn.textContent = 'GenerateSeed';
+    seedBtn.className = 'buttons'
     
     createRemoveButton.style.cursor = 'pointer';
     createRemoveButton.style.width = '25px';
@@ -69,12 +81,17 @@ function createUserCard(data){
     createRemoveButton.style.borderBottom = 'solid';
 
 
-    createRemoveButton.onclick = e => e.target.parentNode.remove()
+    createRemoveButton.onclick = e => e.target.parentNode.remove();
 
-    userCard.append(createRemoveButton, createImg, createName, createEmail, createUsername, createDOB, createNumber, createAddress,  createSeed)
-    newFunction(userCard)
-    cardDiv.append(userCard)
+    userCard.append(createRemoveButton, createImg, createName, createAddress, createEmail, createUsername, createDOB, createNumber, seedBtn);
+    newFunction(userCard);
+    cardDiv.append(userCard);
     
+    seedBtn.addEventListener('click', (e) => {
+        seedBtn.remove();
+        userCard.append(createSeed);
+    })
+
 }
 
 
@@ -82,17 +99,17 @@ function newFunction(card){
 
     card.querySelectorAll('h3').forEach(piece => {
         piece.addEventListener("mouseenter", () => {
-            const editButton = document.createElement('button')
-            editButton.id = "editButton"
-            editButton.textContent = "Edit"
-            piece.appendChild(editButton)
-            editButton.onclick = e => editFormCreation(e)
+            const editButton = document.createElement('button');
+            editButton.id = "editButton";
+            editButton.textContent = "Edit";
+            piece.appendChild(editButton);
+            editButton.onclick = e => editFormCreation(e);
             
         }) 
     })
     card.querySelectorAll("h3").forEach( piece => {
         piece.addEventListener("mouseleave", e => {
-            document.querySelector('#editButton').remove()
+            document.querySelector('#editButton').remove();
         })
     })
 }
@@ -100,27 +117,28 @@ function newFunction(card){
 
 
 function editFormCreation(event) {
-    let changesMade = document.createElement('form')
+    let changesMade = document.createElement('form');
 
-    let edits = document.createElement('input')
-    edits.type = 'text'
-    edits.id = 'myEdits'
-    edits.placeholder = 'Edit Info' 
+    let edits = document.createElement('input');
+    edits.type = 'text';
+    edits.id = 'myEdits';
+    edits.placeholder = 'Edit Info';
 
-    let saveButton = document.createElement('input')
-    saveButton.value = 'Save Changes'
-    saveButton.type = 'submit'  
+    let saveButton = document.createElement('input');
+    saveButton.value = 'Save Changes';
+    saveButton.type = 'submit'; 
 
-    changesMade.append(edits, saveButton)  
-    event.target.appendChild(changesMade)
+    changesMade.append(edits, saveButton);
+    event.target.appendChild(changesMade);
 
     changesMade.addEventListener('submit', e => {
       e.preventDefault();
-      event.target.parentNode.textContent = `${e.target.myEdits.value}`
-      e.target.remove()
+      event.target.parentNode.textContent = `${e.target.myEdits.value}`;
+      e.target.remove();
     })
 }
 
 
 
-fetchRandomUser(api)
+
+
