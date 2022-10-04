c0abf2bd3b9703d41e82acdf198ed76000aa9010
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-10-04 10:33:14 -0600

    most functionality

--- index.js
+++ index.js
@@ -2,17 +2,41 @@ const api = "https://randomuser.me/api/"
 
 
 function fetchRandomUser(url){
-    document.querySelector("#submitButton").onclick = e => {
-        fetch(url)
-        .then(resp => resp.json())
-        .then(data => createUserCard(data))
+    document.querySelector("#submitButton").onclick = () => {
+        const fetch1 = fetch(url)
+        .then(resp => resp.json());
+        const fetch2 = fetch(url)
+        .then(resp => resp.json());
+        const fetch3 = fetch(url)
+        .then(resp => resp.json());
+        const fetch4 = fetch(url)
+        .then(resp => resp.json());
+
+        Promise.all([fetch1, fetch2, fetch3, fetch4])
+        .then(fetchArray => {
+            fetchArray.forEach(createUserCard);
+        })
     }
+}
+
+document.querySelector('#clearUsers').onclick = () => {
+    document.querySelectorAll(".userCards").forEach(card => card.remove())
+}
+
+document.querySelector('#submitSeed').addEventListener('submit', e => fetchSeedUser(e))
+
+function fetchSeedUser(event){
+    event.preventDefault()
+    fetch(`https://randomuser.me/api/?seed=${event.target.seedInput.value}`)
+    .then(res => res.json())
+    .then(data => createUserCard(data))
 }
 
 function createUserCard(data){
 
     const cardDiv = document.getElementById('listContainer')
     const userCard = document.createElement('div')
+    userCard.className = 'userCards'
 
     const createImg = document.createElement('img');
     const createName = document.createElement('h3');
@@ -21,30 +45,36 @@ function createUserCard(data){
     const createDOB = document.createElement('h3');
     const createNumber = document.createElement('h3');
     const createAddress = document.createElement('h3');
-
-    // const seedBtn = document.createElement('button');
-    // const seed = document.createElement('h3');
+    const createSeed = document.createElement('h4')
+    const createRemoveButton = document.createElement('button')
+    createRemoveButton.className = 'removeButton'
 
     createImg.src = data.results[0].picture.large;
     createName.textContent = `${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}`
     createEmail.textContent = `Email:\n${data.results[0].email}`;
-    createUsername.textContent = `Username:\n${data.results[0].login.username}}`;
+    createUsername.textContent = `Username:\n${data.results[0].login.username}`;
     createDOB.textContent = `Age: ${data.results[0].dob.age}\nDOB: ${data.results[0].dob.date.substring(0,10)}`;
     createNumber.textContent = `Phone Number:\n${data.results[0].phone}`;
     createAddress.textContent = `Address:\n${data.results[0].location.street.number} ${data.results[0].location.street.name}, ${data.results[0].location.city}, ${data.results[0].location.state}, ${data.results[0].location.country}, ${data.results[0].location.postcode}`;
+    createSeed.textContent = `Seed: \n${data.info.seed}`
+    createRemoveButton.textContent = 'X'
     
-    // seedBtn.textContent = 'Generate Seed';
-    // seedBtn.className = 'buttons'
-    // seed.textContent = SEED;
+    createRemoveButton.style.cursor = 'pointer';
+    createRemoveButton.style.width = '25px';
+    createRemoveButton.style.height = '25px';
+    createRemoveButton.style.background = 'white';
+    createRemoveButton.style.fontWeight = 'bold'
+    createRemoveButton.style.float = 'right';
+    createRemoveButton.style.borderLeft = 'solid';
+    createRemoveButton.style.borderBottom = 'solid';
 
 
+    createRemoveButton.onclick = e => e.target.parentNode.remove()
 
-    userCard.append(createImg, createName, createEmail, createUsername, createDOB, createNumber, createAddress)
+    userCard.append(createRemoveButton, createImg, createName, createEmail, createUsername, createDOB, createNumber, createAddress,  createSeed)
     newFunction(userCard)
     cardDiv.append(userCard)
     
-
-
 }
 
 
@@ -66,6 +96,8 @@ function newFunction(card){
         })
     })
 }
+
+
 
 function editFormCreation(event) {
     let changesMade = document.createElement('form')
